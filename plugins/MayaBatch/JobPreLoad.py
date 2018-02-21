e9ceb9761e7fb6522d1ee47bcbb819b0ad0acb9c
Toke Jepsen <tokejepsen@gmail.com>
2018-02-21 14:08:34 +0100

    Restructure and add alshaders for cryptomatte.

--- plugins/MayaBatch/JobPreLoad.py
+++ plugins/MayaBatch/JobPreLoad.py
@@ -6,5 +6,11 @@ def __main__(deadlinePlugin):
     deadlinePlugin.LogInfo("Adding custom userSetup.py")
 
     deadlinePlugin.SetProcessEnvironmentVariable(
-        "PYTHONPATH", os.path.dirname(__file__)
+        "PYTHONPATH",
+        os.path.abspath(os.path.join(__file__, "..", "PYTHONPATH"))
+    )
+
+    deadlinePlugin.SetProcessEnvironmentVariable(
+        "ARNOLD_PLUGIN_PATH",
+        os.path.abspath(os.path.join(__file__, "..", "ARNOLD_PLUGIN_PATH"))
     )
