d59a6ae2867d99b4bbd3a03bde5cce712589c673
Toke Stuart Jepsen <tokejepsen@gmail.com>
2024-03-07 16:09:02 +0000

    Multiple jobs for Requeuer event plugin

--- events/Requeuer/Requeuer.py
+++ events/Requeuer/Requeuer.py
@@ -15,6 +15,7 @@ def CleanupDeadlineEventListener(eventListener):
 
 class RequeuerEventListener(DeadlineEventListener):
     def __init__(self):
+        super().__init__()
         self.OnHouseCleaningCallback += self.OnHouseCleaning
 
     def Cleanup(self):
@@ -24,18 +25,25 @@ class RequeuerEventListener(DeadlineEventListener):
         method = RepositoryUtils.GetEventPluginConfigMetaDataDictionary
         self.plugin_metadata = method("Requeuer")
         print("Requeuer Event Plugin - On House Cleaning Started")
-        JobID = self.GetConfigEntryWithDefault("JobID", "")
-        theJob = RepositoryUtils.GetJob(JobID, True)
-        RequeueThreshold = self.GetIntegerConfigEntryWithDefault(
-            "RequeuePeriod", 3600
-        )
-        print("Requeue Threshold in seconds:", RequeueThreshold)
-        print("Job to requeue:", JobID)
 
-        if theJob:
+        configs = ["JobID1", "JobID2"]
+        job_ids = [self.GetConfigEntryWithDefault(x, "") for x in configs]
+
+        configs = ["RequeuePeriod1", "RequeuePeriod2"]
+        periods = [self.GetConfigEntryWithDefault(x, "") for x in configs]
+        for count, job_id in enumerate(job_ids):
+            theJob = RepositoryUtils.GetJob(job_id, True)
+            if not theJob:
+                print("Could not find job with id: {}".format(job_id))
+
+            RequeueThreshold = int(periods[count])
+            print("Requeue Threshold in seconds:", RequeueThreshold)
+            print("Job to requeue:", job_ids[count])
+
             now_time = datetime.now()
             now_time_str = datetime.strftime(now_time, '%Y-%m-%d %H:%M:%S.%f')
-            last_time_str = self.GetMetaDataEntry("LastRequeueTime")
+            metadata_entry = "LastRequeueTime{}".format(count)
+            last_time_str = self.GetMetaDataEntry(metadata_entry)
             if last_time_str is not None:
                 last_time = datetime.strptime(
                     last_time_str, '%Y-%m-%d %H:%M:%S.%f'
@@ -52,13 +60,9 @@ class RequeuerEventListener(DeadlineEventListener):
                 else:
                     print("Requeueing Job!")
                     RepositoryUtils.RequeueJob(theJob)
-                    if not self.plugin_metadata.ContainsKey("LastRequeueTime"):
-                        self.AddMetaDataEntry("LastRequeueTime", now_time_str)
+                    if not self.plugin_metadata.ContainsKey(metadata_entry):
+                        self.AddMetaDataEntry(metadata_entry, now_time_str)
                     else:
-                        self.UpdateMetaDataEntry(
-                            "LastRequeueTime", now_time_str
-                        )
+                        self.UpdateMetaDataEntry(metadata_entry, now_time_str)
             else:
                 print("Wait Time between Requeues has not elapsed yet...")
-        else:
-            print("Job not found.")
